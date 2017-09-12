6ea2aa59cf6d6ba27e18b67b53fc2c88b8034c16
Sidhra <sidhra.cader@hotmail.com>
2017-09-12 21:03:32 +1000

    created function to check winner

--- gameapp.js
+++ gameapp.js
@@ -1,40 +1,47 @@
 console.log("Tic - Tac - Toe");
 
-var board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]];
+var board = [["", "", ""], ["", "", ""], ["", "", ""]];
 
 var rowPlace = 0;
 var colPlace = 0;
-var i = 0;
+var turn = 0;
 var ties = 0;
 var xWins = 0;
 var oWins = 0;
 
 var twoPlayers = function(event){
-  i++;
-  var position = event.target.textContent.split("");
-  var row = Number(position[0]) - 1;
-  var col = Number(position[1]) - 1;
-  if (i % 2 ===1) {
-    board[row][col] = "X";
-    event.target.textContent = board[row][col];
-  } else {
-    board[row][col] = "O";
-    event.target.textContent = board[row][col];
+  turn++;
+  var row = event.target.parentElement.dataset.positionX;
+  var col = event.target.parentElement.dataset.positionY;
+  var player = "O";
+  if (turn % 2 === 1) {
+    player = "X";
   }
-  event.target.style.color = "black";
-  checkWinner();
+  board[row][col] = player;
+  event.target.parentElement.innerHTML = "<span>" + player + "</span>";
+  var result = checkWinner(player);
 }
 
-var checkWinner = function(){
-  if ((board[0][0] === "X" && board[0][1] === "X" && board[0][2] === "X")) {
-    console.log("winner");
-  } else if ((board[0][0] === "O" && board[0][1] === "O" && board[0][2] === "O")) {
-    console.log("winner");
-  } else if ((board[1][0] === "X" && board[1][1] === "X" && board[1][2] === "X")) {
-    console.log("winner");
-  } else if ((board[1][0] === "O" && board[1][1] === "O" && board[1][2] === "O")) {
-    console.log("winner");
+var checkWinner = function(player){
+  for (var i = 0; i < board.length; i++) {
+    if ((board[i][0] === player && board[i][1] === player && board[i][2] === player)) {
+      console.log("winner");
+      return true;
+    }
+    if ((board[0][i] === player && board[1][i] === player && board[2][i] === player)) {
+      console.log("winner");
+      return true;
+    }
   }
+  if ((board[0][0] === player && board[1][1] === player && board[2][2] === player)) {
+    console.log("winner");
+    return true;
+  }
+  if ((board[0][2] === player && board[1][1] === player && board[2][0] === player)) {
+    console.log("winner");
+    return true;
+  }
+  return false;
 }
 
 // var onePlayer = function(event){
@@ -65,12 +72,17 @@ var gameBoard = document.querySelector(".board");
 var cloneBoard = gameBoard.innerHTML;
 var resetButton = document.querySelector("button");
 
-gameBoard.addEventListener("click", twoPlayers);
+
 
 var resetBoard = function(){
-  i = 0;
-  var board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]];
+  turn = 0;
+  board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]];
   gameBoard.innerHTML = cloneBoard;
+  var gameBoardSquares = document.querySelectorAll(".board a")
+  gameBoardSquares.forEach(function(square){
+    square.addEventListener("click", twoPlayers);
+  });
 }
 
+resetBoard();
 resetButton.addEventListener("click", resetBoard);
